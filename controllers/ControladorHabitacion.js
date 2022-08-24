688071bb38fb0478b881be610d36b2961acb794f
201 <201@academicos.cesde.edu.co>
2022-08-23 20:31:45 -0500

    se despliega pproyecto

---
+++ controllers/ControladorHabitacion.js
@@ -0,0 +1,107 @@
+import {ServicioHabitacion} from '../services/ServicioHabitacion.js'
+
+export class ControladorHabitacion{
+
+
+    constructor(){}
+
+    //buscar habitaciones
+    buscarHabitaciones(request,response){
+
+        //llamo al servicio
+        let servicioHabitacion=new ServicioHabitacion()
+
+        //Intento resolver la PETICION
+        try{
+           response.status(200).json({
+            mensaje:"exito en la consulta",
+            datos:servicioHabitacion.buscarTodas()
+           }) 
+        }catch(error){ //FALLO RESOLVIENDO LA PETICION
+            response(400).json({
+                mensaje:"fallo en la consulta "+error,
+                datos:null
+            })
+        }
+    }
+
+    //buscar habitacion por id
+    buscarHabitacionPorId(request,response){
+        let identificador=request.params.id
+        
+        //Llamo al servicio habitaciones
+        let servicioHabitacion=new ServicioHabitacion()
+
+        try{
+            response.status(200).json({
+                mensaje:"exito en la consulta "+identificador,
+                datos: servicioHabitacion.buscarPorId(identificador)
+            }) 
+         }catch(error){ //FALLO RESOLVIENDO LA PETICION
+             response(400).json({
+                mensaje:"fallo en la consulta "+error,
+                datos:null
+             })
+         }
+    }
+
+    //agregar habitacion
+    agregarHabitacion(request,response){
+        let cuerpo=request.body
+
+        //Llamo al servicio habitaciones
+        let servicioHabitacion=new ServicioHabitacion()
+        
+        try{
+            servicioHabitacion.agregar(cuerpo)
+            response.status(200).json({
+                mensaje:"exito agregando la habitacion",
+                datos:null
+            }) 
+         }catch(error){ //FALLO RESOLVIENDO LA PETICION
+             response(400).json({
+                mensaje:"fallo en la consulta "+error,
+                datos:null
+             })
+         }
+    }
+
+    //editar habitacion
+    editarHabitacion(request,response){
+
+        //recibir id como parametro
+        let id=request.params.id
+
+        //recibir los datos con los que voy a editar (BODY)
+        let datos=request.body
+
+        //Llamo al servicio habitaciones
+        let servicioHabitacion=new ServicioHabitacion()
+
+        try{
+            servicioHabitacion.actualizar(id,datos)
+            response.status(200).json({
+                mensaje:"exito editando la habitacion "+id,
+                datos:null
+            }) 
+         }catch(error){ //FALLO RESOLVIENDO LA PETICION
+             response(400).json({
+                mensaje:"fallo en la consulta "+error,
+                datos:null
+             })
+         }
+
+    }
+
+    //eliminar habitacion
+    eliminarHabitacion(request,response){
+        try{
+            response.status(200).json({}) 
+         }catch(error){ //FALLO RESOLVIENDO LA PETICION
+             response(400).json({})
+         }
+    }
+
+
+
+}
---
+++ services/ServicioHabitacion.js
@@ -0,0 +1,32 @@
+import {modeloHabitacion} from '../models/modeloDatosHabitacion.js'
+
+export class ServicioHabitacion{
+
+    constructor(){}
+
+    async buscarTodas(){
+        let habitaciones=await modeloHabitacion.find()
+        return habitaciones
+    }
+
+    async buscarPorId(id){
+        let habitacion=await modeloHabitacion.findById(id)
+        return habitacion
+    }
+
+    async agregar(datos){
+
+        let habitacionAGuardar=new modeloHabitacion(datos)
+        return await habitacionAGuardar.save()
+
+    }
+
+    async actualizar(id,datos){
+
+        return modeloHabitacion.findByIdAndUpdate(id,datos)
+
+    }
+
+
+
+}
